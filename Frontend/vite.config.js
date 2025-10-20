09a4a780cacd210855dc783e52b36ed142c5b345
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2025-10-21 00:19:17 +0530

    Modify the vite config and db.json file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(),],
-  base: "/Practice-project-2-with-React/",
+  base: "/Practice-project-2-with-React",
 });
